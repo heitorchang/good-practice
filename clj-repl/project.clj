84a472d1f54365c2bb3335d650d46bffb10cbd8d
Heitor Chang <heitor.chang@tempook.com>
2022-02-23 14:12:58 -0300

    project reference

--- clj-repl/project.clj
+++ clj-repl/project.clj
@@ -1,11 +1,14 @@
 (defproject com.heitorchang/cljrepl "0.0.1"
   :description "clj-repl"
   :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/math.combinatorics "0.1.6"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
   :repl-options {:init-ns clj-repl
                  :init (do
                          (set! *print-length* 100)
                          (require '[clojure.string :as str]
+                                  '[clojure.set :as cset]   ;; https://clojure.org/guides/repl/navigating_namespaces
+                                  '[clojure.math.combinatorics :as combo]
                                   '[clojure.math.numeric-tower :as math]))})
 
 ;; WAIT for REPL to load
